ad2b6e7c0e6b94868cf7b34a99a9b91d3361d7ed
melodynnadi <melodynnadi2@gmail.com>
2025-03-20 01:19:35 -0500

    yh

--- script.js
+++ script.js
@@ -59,4 +59,22 @@ function moveSlide(direction) {
   });
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.getElementById('hamburger-menu');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', function() {
+      hamburger.classList.toggle('active');
+      navMenu.classList.toggle('active');
+  });
+
+  // Close menu when a link is clicked
+  document.querySelectorAll('.nav-menu a').forEach(n => n.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navMenu.classList.remove('active');
+  }));
+});
+
+
   
